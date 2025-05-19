85b73992ef9bb647f3899af71de3eb86ee39d963
zinevych-pja <164073761+zinevych-pja@users.noreply.github.com>
2025-05-19 13:44:02 +0200

    Cw 19.05

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float runSpeed = 7;
+    public float jumpForce = 10;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        //Debug.Log($"Input pressed: {moveInput}");
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.velocity = new Vector2(moveInput * runSpeed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //rb.AddForce(new Vector2(0,jumpForce));
+            rb.AddForce( Vector2.up * jumpForce );
+        }
+    }
+}
